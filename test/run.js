e2562a403630792c8a278029d130b05970d7d875
Roman Komarov <kizu@yandex-team.ru>
2016-12-18 14:29:24 +0300

    v2.0.0 — bemto.pug

--- test/run.js
+++ test/run.js
@@ -2,26 +2,26 @@
  * Module dependencies.
  */
 
-var jade = require('jade')
+var pug = require('pug')
   , fs = require('fs');
 
 // test cases
 
 var cases = fs.readdirSync('test/cases').filter(function(file){
-  return ~file.indexOf('.jade');
+  return ~file.indexOf('.pug');
 }).map(function(file){
-  return file.replace('.jade', '');
+  return file.replace('.pug', '');
 });
 
 cases.forEach(function(test){
   var name = test.replace(/[-.]/g, ' ');
   if (!fs.existsSync('test/cases/' + test + '.html')) return;
   it(name, function(){
-    var path = 'test/cases/' + test + '.jade';
+    var path = 'test/cases/' + test + '.pug';
     var str = fs.readFileSync(path, 'utf8');
     var html = fs.readFileSync('test/cases/' + test + '.html', 'utf8').trim().replace(/\r/g, '');
-    var fn = jade.compile(str, { filename: path, pretty: true });
-    var actual = fn({ title: 'Jade' });
+    var fn = pug.compile(str, { filename: path, pretty: true });
+    var actual = fn({ title: 'Pug' });
     actual.trim().should.equal(html);
   })
 });
